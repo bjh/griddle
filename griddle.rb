bdeae90db84d53d4409bb677b79ee76624f38a86
bjh <brodie.hanbuch@gmail.com>
2013-02-07 21:08:48 -0800

    edited readme

--- griddle.rb
+++ griddle.rb
@@ -3,9 +3,14 @@ require_relative 'lib/data_store.rb'
 data_store = DataStore.new('data/po.csv')
 
 puts "Vendor"
-puts data_store.cut(11, 1, 1, 5)
+candy = data_store.cut(11, 1, 1, 5)
+puts candy
 puts
 puts "Ship To"
 shipto = data_store.find("SHIP TO")
+puts shipto
 puts data_store.cut(shipto.row+1, shipto.col, 1, 5)
 
+data_store.cut(21, 1, 7, 2).each do |row|
+  puts row.join(":")
+end
